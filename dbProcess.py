429b52887c969bf2afb788679836a4a9df85e049
cvegao <constanza.vega@uv.es>
2022-04-27 01:05:07 +0200

    boxplot in progress

--- dbProcess.py
+++ dbProcess.py
@@ -1,19 +1,32 @@
-# import matplotlib
-import csv
+import matplotlib.pyplot as plt
+
+import scipy.stats
+import pandas as pd
 
 
-def dataCharge(fileName='Ramet_number.csv'):
-
-    file = open(fileName)
-    data = csv.reader(file)
-
-    header = next(data)
-    body = []
-
-    for row in data:
-        body.append(row)
-
-    file.close()
+def data_loading(file_name='Ramet_number.csv'):
+    """
+    :return: DataFrame
+    """
+    data = pd.read_csv(file_name)
+    return data
 
 
-print(body)
+def is_normal(data, rows):
+    result = scipy.stats.shapiro(data[rows, ])
+    print(result)
+
+
+df = data_loading()
+
+# fig, ax = plt.subplots()
+# colors = {'LS': 'red', 'HS': 'blue'}
+
+# ax.scatter(df['Plant'], df['RametNumber'], c=df['snail'].map(colors))
+
+filtered_df = df[df["SampleDay"] == 6]
+
+filtered_df.boxplot(by=["Plant", "snail"], column="RametNumber")
+
+
+plt.show()
