d8ee13619cdb00de587bd47281869c642eab2c85
cvegao <constanza.vega@uv.es>
2022-04-27 23:52:27 +0200

    stats in progress

--- dbProcess.py
+++ dbProcess.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 import scipy.stats
 import pandas as pd
+from scipy import stats
 
 
 def data_loading(file_name='Ramet_number.csv'):
@@ -19,14 +20,27 @@ def is_normal(data, rows):
 
 df = data_loading()
 
-# fig, ax = plt.subplots()
-# colors = {'LS': 'red', 'HS': 'blue'}
-
-# ax.scatter(df['Plant'], df['RametNumber'], c=df['snail'].map(colors))
-
+"""
 filtered_df = df[df["SampleDay"] == 6]
 
-filtered_df.boxplot(by=["Plant", "snail"], column="RametNumber")
+filtered_df.boxplot(by=["Plant", "snail"], column="PVI")
 
-
+plt.ylabel("PVI")
 plt.show()
+"""
+myLS = df[df["Plant"] == "My"]
+myLS = myLS[myLS["snail"] == "LS"]
+myLS = myLS[myLS["SampleDay"] == 6]
+myHS = df[df["Plant"] == "My"]
+myHS = myHS[myHS["snail"] == "HS"]
+myHS = myHS[myHS["SampleDay"] == 6]
+print(myLS, myHS, sep="\n")
+print(f"My: {stats.kruskal(myLS['PVI'], myHS['PVI'])}")
+
+pbLS = df[df["Plant"] == "PB"]
+pbLS = pbLS[pbLS["snail"] == "LS"]
+pbLS = pbLS[pbLS["SampleDay"] == 6]
+pbHS = df[df["Plant"] == "PB"]
+pbHS = pbHS[pbHS["snail"] == "HS"]
+pbHS = pbHS[pbHS["SampleDay"] == 6]
+print(f"PB: {stats.kruskal(pbLS['PVI'], pbHS['PVI'])}")
